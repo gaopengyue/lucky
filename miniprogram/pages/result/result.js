7059375707b255e7f7fa32dd3d41aaa25203bd4e
gaopengyue <gaofancyy@163.com>
2019-03-16 11:08:35 +0800

    add:xingzuo

--- miniprogram/pages/result/result.js
+++ miniprogram/pages/result/result.js
@@ -16,64 +16,25 @@ Page({
     wx.getStorage({
       key: 'docItem',
       success: res => {
-        let list = []
-        res.data.case.forEach(item => {
-          list = list.concat(item.split(/\n/g))
+        let arr = []
+        res.data.case.forEach((item, index) => {
+          let d = {}
+          if (index % 2 == 0) {
+            d.title = item
+          } else {
+            d.content = item.split(/\n/g)
+          }
+          arr.push(d)
         })
         this.setData({
           docItem: res.data,
-          caseList: list
+          caseList: arr
+        })
+        console.log(arr)
+        wx.setNavigationBarTitle({
+          title: `${res.data.sort}`,
         })
       }
     })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
